483c4b62cdc2b5b9ca6b580d7492cc69e5476c0b
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-05 01:17:16 +0530

    handle response

--- databaseConnected/src/main/java/lk/ijse/databaseConnected/controller/ProductController.java
+++ databaseConnected/src/main/java/lk/ijse/databaseConnected/controller/ProductController.java
@@ -3,6 +3,8 @@ package lk.ijse.databaseConnected.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,13 +25,22 @@ public class ProductController {
     }
 
     @PostMapping("/products")
-    public Product createProduct(@RequestBody Product product){
-        return productService.createProduct(product);
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        try{
+            return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(product));
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 
     @GetMapping("/products/{id}")
-    public Product getProductById(@PathVariable Long id){
-        return productService.getProductById(id);
+    public ResponseEntity<Product> getProductById(@PathVariable Long id){
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(product);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
     @PostMapping("/products/{id}")
