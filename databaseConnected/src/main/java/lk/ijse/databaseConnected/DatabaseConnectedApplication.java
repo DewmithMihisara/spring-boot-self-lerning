e4ae998bd019e0dd0ae04f2c498181309d86103c
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-04 15:11:51 +0530

    add project for database connection

---
+++ databaseConnected/src/main/java/lk/ijse/databaseConnected/DatabaseConnectedApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.databaseConnected;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseConnectedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseConnectedApplication.class, args);
+	}
+
+}
---
+++ databaseConnected/src/test/java/lk/ijse/databaseConnected/DatabaseConnectedApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.databaseConnected;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseConnectedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
