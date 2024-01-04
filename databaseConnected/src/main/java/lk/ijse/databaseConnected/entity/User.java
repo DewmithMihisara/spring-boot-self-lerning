82e15b4c9221cfae602ae0a4b32a8ac13d62a3d2
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-01-04 19:47:04 +0530

    add entity

---
+++ databaseConnected/src/main/java/lk/ijse/databaseConnected/entity/User.java
@@ -0,0 +1,30 @@
+package lk.ijse.databaseConnected.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.GenerationType;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
